7c0dd0aba5aa295ac661c4b1c5e06a5c0ca127c7
goel03 <95127071+goel03@users.noreply.github.com>
2023-08-10 11:48:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
+const app = express();
+const cors = require("cors");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+dotenv.config();
+
+app.use(express.json());
+
+const userRoutes = require("./Routes/userRoutes");
+const chatRoutes = require("./Routes/chatRoutes");
+const messageRoutes = require("./Routes/messageRoutes");
+
+
+
+
+const connectDb = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log("Server is Connected to Database");
+  } catch (err) {
+    console.log("Server is NOT connected to Database", err.message);
+  }
+};
+connectDb();
+
+app.get("/", (req, res) => {
+  res.send("API is running123");
+});
+
+app.use("/user", userRoutes);
+app.use("/chat", chatRoutes);
+app.use("/message", messageRoutes);
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+//sstattic files
+app.use(express.static(path.join(__dirname,'./live-chat-client/build')));
+app.get('*',function(req,res)
+{
+  res.sendFile(path.join(__dirname,'./live-chat-client/build/index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log("Server is Running..."));
